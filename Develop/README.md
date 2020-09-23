04d7ddc40a7ea5c7f49be01105b012d02f0e9e4c
Maria Byler <mariabyler98@gmail.com>
2020-09-22 22:40:22 -0400

    added character selection via user input

---
+++ Develop/README.md
@@ -0,0 +1,2 @@
+This project was submitted as homework for the UNC full stack coding bootcamp. With HTML provided, our assignment was to write Javascript to generate a password with user input to determine length and character types of password. 
+![image](https://user-images.githubusercontent.com/69592367/93945021-1dcea580-fd04-11ea-8a1f-4432a30fe7f0.png)
--- Develop/script.js
+++ Develop/script.js
@@ -2,50 +2,93 @@
 var generateBtn = document.querySelector("#generate");
 var passwordPrompt = prompt("Choose a password length between 8-128 characters");
 var passwordLength = parseInt(passwordPrompt, 10);
-var characters = prompt("Choose lowercase, uppercase, numeric, or special characters"); 
 
-console.log(passwordLength); 
-console.log(passwordPrompt); 
-console.log(characters); 
+console.log(passwordLength, "password length"); 
+console.log(passwordPrompt, "password prompt"); 
 
 // prompt asking for password length
 function confirmLength() {
   if (passwordLength <= 128 && passwordLength >= 8) {
     alert("Thank you"); 
    } else {
-     parseInt(prompt("Choose a password length between 8-128 characters")); 
+     alert("Error, please choose a password length between 8-128 characters");
    } 
 }
 
 confirmLength (); 
 
-// prompt asking for characters BROKEN 
-function confirmCharacters () {
-  if (characters === "lowercase" || "uppercase" || "numeric" || "special characters") {
-    alert("Thank you"); 
-   } else {
-     prompt("Choose lowercase, uppercase, numeric, or special characters"); 
-   }
-}
+// character variables are down here so they will load after an error message in the event of user inputing a value not between 8-128
+var lowercase = prompt("Would you like lowercase letters?");  
+    if (lowercase == "yes") {
+      lowercase = true; 
+    } else {
+      lowercase = false; 
+    }
 
-confirmCharacters(); 
+var uppercase = prompt("Would you like uppercase letters?"); 
+    if (uppercase == "yes") {
+      uppercase = true; 
+    } else {
+      uppercase = false; 
+    }
+
+var special = prompt("Would you like special characters?"); 
+    if (special == "yes") {
+      special = true; 
+    } else {
+      special = false; 
+    }
+    
+var numeric = prompt("Would you like numbers?");
+    if (numeric == "yes") {
+      numeric = true; 
+    } else {
+      numeric = false; 
+    }
+
+console.log(lowercase, "uppercase");
+console.log(uppercase, "uppercase");
+console.log(special, "special");
+console.log(numeric, "numeric");
 
 // function to generate password
-function generatePassword() {
-  var length = passwordLength,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        password = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        password += charset.charAt(Math.floor(Math.random() * n));
+function generatePassword(charset) { 
+  charset = charset.toString(); 
+  console.log(charset, "charset"); 
+       var password = "";
+      for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * charset.length);  
+        password = password + charset[randomNumber]; 
+        console.log(password); 
+        console.log(randomNumber); 
     }
-    return password;
+    password = password.toString(); 
+    return password; 
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // conditionals for alerts
+  var charset = []; 
+  if(lowercase == true) {
+    var lowercaseChar="abcdefghijklmnopqrstuvwxyz"; 
+    charset.push(lowercaseChar);  
+  } 
+  if (uppercase == true) {
+    var uppercaseChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    charset.push(uppercaseChar); 
+  } 
+  if (numeric == true) { 
+    var numericChar="0123456789"; 
+    charset.push(numericChar); 
+  } 
+  if (special == true) { 
+    var specialChar="!@#$%^&*?"; 
+    charset.push(specialChar); 
+  }
 
+  var password = generatePassword(charset); 
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
